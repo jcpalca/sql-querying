0cca8feb17621a657a7bedd3f390025a12b92742
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-17 19:16:38 -0700

    Add chair to products table

--- queries_products.sql
+++ queries_products.sql
@@ -1,1 +1,9 @@
 -- Comments in SQL Start with dash-dash --
+
+-- Add a product to the table with the name of "chair",
+--  price of 44.00, and can_be_returned of false.
+
+INSERT INTO products
+  (name, price, can_be_returned)
+  VALUES
+    ('chair', 44.00, false);
