4d02319fa9738644b9910952ee8f2da904042fa2
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-17 19:27:17 -0700

    Add stool and table to products table

--- queries_products.sql
+++ queries_products.sql
@@ -7,3 +7,19 @@ INSERT INTO products
   (name, price, can_be_returned)
   VALUES
     ('chair', 44.00, false);
+
+-- Add a product to the table with the name of "stool",
+--  price of 25.99, and can_be_returned of true.
+
+INSERT INTO products
+  (name, price, can_be_returned)
+  VALUES
+    ('stool', 25.99, TRUE);
+
+-- Add a product to the table with the name of "table", price of 124.00,
+--  and can_be_returned of false.
+
+INSERT INTO products
+  (name, price, can_be_returned)
+  VALUES
+    ('table', 124.00, FALSE);
